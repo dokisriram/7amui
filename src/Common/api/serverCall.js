e42a52bd2893aab5659e6177d4545ee44f12cedd
Doki Sriram <dokisriram@gmail.com>
2024-06-02 23:26:26 +0530

    student table and pagination

--- src/Common/api/serverCall.js
+++ src/Common/api/serverCall.js
@@ -1,17 +1,23 @@
+import { Cookie } from "./cookies";
+
 const { default: axios } = require("axios");
 
 
 export class ServerCall{
     static  sendGetReq(url){
-        return axios.get(process.env.BASE_URL+url)
+        return axios.get(process.env.NEXT_PUBLIC_BASE_URL+url, {
+            headers:{
+                Authorization: Cookie.getCookie('token')
+            }
+        })
     } 
     static sendPostReq(url, data){
-        return axios.post(process.env.BASE_URL+url, data)
+        return axios.post(process.env.NEXT_PUBLIC_BASE_URL+url, data)
     }
     static sendPutReq(url, data){
-        return axios.put(process.env.BASE_URL+url, data)
+        return axios.put(process.env.NEXT_PUBLIC_BASE_URL+url, data)
     }
     static sendDeleteReq(url){
-        return axios.delete(process.env.BASE_URL+url)        
+        return axios.delete(process.env.NEXT_PUBLIC_BASE_URL+url)        
     }
 }
--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -1,19 +1,39 @@
+"use client"
 import React, { useEffect, useState } from 'react'
 import styles from './Students.module.css'
 import { ServerCall } from '@/Common/api/serverCall';
+import Table from '@/Common/reusableComponents/Table/Table';
+import { useDispatch } from 'react-redux';
 
 
 const Students = () => {
-    const [students, setStudents] = useState([]);
-    useEffect(() => {
+  const [students, setStudents] = useState([]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fnGetStudents = async () => {
+      try {
+        dispatch({ type: 'LOADER', payload: true })
+        const res = await ServerCall.sendGetReq('std/get-std')
+        if (Array.isArray(res?.data)) {
+          setStudents(res?.data);
+        }
+      } catch (error) {
 
-    }, [])
-    const fnGetStudents = () => {
-        ServerCall.sendGetReq('std/get-std')
+      } finally {
+        dispatch({ type: 'LOADER', payload: false })
+      }
     }
+    fnGetStudents();
+  }, [dispatch])
+
   return (
     <div>
-      
+      <h3 className='my-3 text-center'>Students</h3>
+      <Table
+        headers={['NAME', 'UID', 'ROLL NO', 'ADDRESS']}
+        rows={students}
+        tds={['Name', 'uid', 'rno', 'address']}
+      />
     </div>
   )
 }
